4fbec1a4f8425fba13bf9fd34e9a9b28d4dc23a7
Emil Yessenamanov <emil@doo.net>
2013-11-05 14:46:21 +0100

    Create lein project.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.doo/bandalore "0.0.6-SNAPSHOT" 
+  :parent [org.clojure/pom.contrib "0.0.20"]
+  :dependencies [[com.amazonaws/aws-java-sdk "1.6.4"]]
+  :url "http://github.com/cemerick/bandalore"
+  :description "A Clojure library for Amazon's Simple Queue Service (SQS)."
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"])
